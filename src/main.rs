464d9b9a5bfe3e46e2c2dbb945261b10c96864ed
Linda Siemons <linda@catbrained.dev>
2024-12-03 01:18:02 +0100

    feat: solve challenge -1, task 2
    Signed-off-by: Linda Siemons <linda@catbrained.dev>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use axum::{
+    extract::Path,
+    http::{header, HeaderName, StatusCode},
+    response,
     routing::get,
     Router,
 };
@@ -7,10 +10,22 @@ async fn hello_world() -> &'static str {
     "Hello, bird!"
 }
 
+async fn seek(
+    Path(id): Path<i8>,
+) -> response::Result<(StatusCode, [(HeaderName, &'static str); 1])> {
+    let song_url = match id {
+        -1 => "https://www.youtube.com/watch?v=9Gc4QTqslN4",
+        _ => return Err(StatusCode::NOT_FOUND.into()),
+    };
+
+    Ok((StatusCode::FOUND, [(header::LOCATION, song_url)]))
+}
+
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
     let router = Router::new()
-        .route("/", get(hello_world));
+        .route("/", get(hello_world))
+        .route("/:id/seek", get(seek));
 
     Ok(router.into())
 }
