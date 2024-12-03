1ddaad900380e9ae21b62f13d0f2d206ac738ddd
Linda Siemons <linda@catbrained.dev>
2024-12-03 05:01:56 +0100

    feat: solve challenge 2, task 2
    Signed-off-by: Linda Siemons <linda@catbrained.dev>

--- src/main.rs
+++ src/main.rs
@@ -48,12 +48,35 @@ async fn ip_decrypt(ipdec: Query<IpDec>) -> String {
     dest.to_string()
 }
 
+#[derive(Deserialize)]
+struct IpGetKey {
+    from: Ipv4Addr,
+    to: Ipv4Addr,
+}
+
+async fn ip_get_key(getkey: Query<IpGetKey>) -> String {
+    let getkey = getkey.0;
+    let f = getkey.from.octets();
+    let t = getkey.to.octets();
+
+    let key: Ipv4Addr = [
+        t[0].wrapping_sub(f[0]),
+        t[1].wrapping_sub(f[1]),
+        t[2].wrapping_sub(f[2]),
+        t[3].wrapping_sub(f[3]),
+    ]
+    .into();
+
+    key.to_string()
+}
+
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
     let router = Router::new()
         .route("/", get(hello_world))
         .route("/:id/seek", get(seek))
-        .route("/2/dest", get(ip_decrypt));
+        .route("/2/dest", get(ip_decrypt))
+        .route("/2/key", get(ip_get_key));
 
     Ok(router.into())
 }
