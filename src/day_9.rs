9e5c03e6a809ca584890fa5a7f17b2ef8b9c61ac
Linda Siemons <linda@catbrained.dev>
2024-12-19 23:05:20 +0100

    feat: solve challenge 9, task 2
    Signed-off-by: Linda Siemons <linda@catbrained.dev>

--- src/day_9.rs
+++ src/day_9.rs
@@ -1,8 +1,17 @@
 use std::sync::Arc;
 
-use axum::{extract::State, http::StatusCode, routing::post, Router};
+use axum::{
+    extract::{FromRequest, Request, State},
+    http::{header::CONTENT_TYPE, HeaderMap, StatusCode},
+    response::{IntoResponse, Response, Result},
+    routing::post,
+    Json, Router,
+};
 use leaky_bucket::RateLimiter;
+use serde::{Deserialize, Serialize};
 use tokio::time::Duration;
+
+static GALLONS_PER_LITER: f32 = 0.264172;
 
 pub fn day_nine() -> Router {
     let bucket = Arc::new(
@@ -22,13 +31,43 @@ pub fn day_nine() -> Router {
 #[derive(Clone)]
 struct BucketState(Arc<RateLimiter>);
 
-async fn milk(State(state): State<BucketState>) -> (StatusCode, String) {
-    if state.0.try_acquire(1) {
-        (StatusCode::OK, "Milk withdrawn\n".to_owned())
-    } else {
-        (
+#[derive(Deserialize, Serialize, Copy, Clone)]
+#[serde(rename_all = "lowercase")]
+enum MilkRequest {
+    Liters(f32),
+    Gallons(f32),
+}
+
+impl MilkRequest {
+    fn convert(self) -> Self {
+        match self {
+            MilkRequest::Liters(l) => Self::Gallons(l * GALLONS_PER_LITER),
+            MilkRequest::Gallons(g) => Self::Liters(g / GALLONS_PER_LITER),
+        }
+    }
+}
+
+async fn milk(
+    State(state): State<BucketState>,
+    headers: HeaderMap,
+    request: Request,
+) -> Result<Response> {
+    if !state.0.try_acquire(1) {
+        return Ok((
             StatusCode::TOO_MANY_REQUESTS,
             "No milk available\n".to_owned(),
         )
+            .into_response());
+    }
+
+    match headers.get(CONTENT_TYPE).map(|hv| hv.to_str()) {
+        Some(Ok("application/json")) => {
+            let request: Json<MilkRequest> = Json::from_request(request, &state)
+                .await
+                .map_err(|_| StatusCode::BAD_REQUEST)?;
+
+            Ok(Json(request.convert()).into_response())
+        }
+        _ => Ok((StatusCode::OK, "Milk withdrawn\n".to_owned()).into_response()),
     }
 }
