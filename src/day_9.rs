b7e2a131c6179b497ffde6a6e3a146559a1a3e8b
Linda Siemons <linda@catbrained.dev>
2024-12-19 23:33:02 +0100

    feat: solve challenge 9, task 4
    Signed-off-by: Linda Siemons <linda@catbrained.dev>

--- src/day_9.rs
+++ src/day_9.rs
@@ -9,28 +9,33 @@ use axum::{
 };
 use leaky_bucket::RateLimiter;
 use serde::{Deserialize, Serialize};
-use tokio::time::Duration;
+use tokio::{sync::RwLock, time::Duration};
 
 static GALLONS_PER_LITER: f32 = 0.264172;
 static PINTS_PER_LITRE: f32 = 1.759754;
 
 pub fn day_nine() -> Router {
-    let bucket = Arc::new(
+    let bucket = BucketState(Arc::new(RwLock::new(BucketState::refill())));
+
+    Router::new()
+        .route("/9/milk", post(milk))
+        .route("/9/refill", post(refill))
+        .with_state(bucket)
+}
+
+#[derive(Clone)]
+struct BucketState(Arc<RwLock<RateLimiter>>);
+
+impl BucketState {
+    fn refill() -> RateLimiter {
         RateLimiter::builder()
             .max(5)
             .initial(5)
             .refill(1)
             .interval(Duration::from_secs(1))
-            .build(),
-    );
-
-    let state = BucketState(bucket);
-
-    Router::new().route("/9/milk", post(milk)).with_state(state)
+            .build()
+    }
 }
-
-#[derive(Clone)]
-struct BucketState(Arc<RateLimiter>);
 
 #[derive(Deserialize, Serialize, Copy, Clone)]
 #[serde(rename_all = "lowercase")]
@@ -57,7 +62,7 @@ async fn milk(
     headers: HeaderMap,
     request: Request,
 ) -> Result<Response> {
-    if !state.0.try_acquire(1) {
+    if !state.0.read().await.try_acquire(1) {
         return Ok((
             StatusCode::TOO_MANY_REQUESTS,
             "No milk available\n".to_owned(),
@@ -75,4 +80,11 @@ async fn milk(
         }
         _ => Ok((StatusCode::OK, "Milk withdrawn\n".to_owned()).into_response()),
     }
+}
+
+async fn refill(State(state): State<BucketState>) -> StatusCode {
+    let mut bucket = state.0.write().await;
+    *bucket = BucketState::refill();
+
+    StatusCode::OK
 }
