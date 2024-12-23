f6f182919afcc090b0bed584368c113bfd5e5eb3
Linda Siemons <linda@catbrained.dev>
2024-12-23 13:29:52 +0100

    feat: solve challenge 23, task 2
    Signed-off-by: Linda Siemons <linda@catbrained.dev>

--- src/day_23.rs
+++ src/day_23.rs
@@ -1,6 +1,12 @@
-use axum::Router;
+use axum::{routing::get, Router};
 use tower_http::services::ServeDir;
 
 pub fn day_twentythree() -> Router {
-    Router::new().nest_service("/assets", ServeDir::new("assets"))
+    Router::new()
+        .nest_service("/assets", ServeDir::new("assets"))
+        .route("/23/star", get(star))
+}
+
+async fn star() -> String {
+    r#"<div id="star" class="lit"></div>"#.to_string()
 }
